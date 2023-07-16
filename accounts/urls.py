4f9986dc39a9966cd6b9ca08f45ae1bc1d7b152b
Kinan wehba <Kinan.itm@gmail.com>
2023-07-16 20:39:16 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   accounts/__pycache__/models.cpython-311.pyc
    	modified:   accounts/__pycache__/urls.cpython-311.pyc
    	modified:   accounts/__pycache__/views.cpython-311.pyc
    	new file:   accounts/migrations/0002_alter_profile_price.py
    	new file:   accounts/migrations/__pycache__/0002_alter_profile_price.cpython-311.pyc
    	deleted:    accounts/templates/user/app.html
    	new file:   accounts/templates/user/restaurants_list.html
    	modified:   accounts/urls.py
    	modified:   accounts/views.py
    	modified:   db.sqlite3

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path ,include
 from . import views
 app_name = 'accounts'
 urlpatterns = [
-    path('app/',views.app ,name= 'app'),
+    path('restaurants/',views.restaurants_list ,name= 'restaurants_list'),
 
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
-def app(request):
-    return render(request ,'user/app.html',{})
+def restaurants_list(request):
+    restaurants = User.objects.all()
+    
+    return render(request ,'user/restaurants_list.html',{
+        "restaurants" : restaurants,
+        })
